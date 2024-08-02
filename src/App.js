acf84dfd72f2ec6eb87a66bf9dd2810774e9d117
yingqi955 <yingqi.ch@gmail.com>
2024-08-02 18:10:12 -0400

    Changing the package.json file to activate npm start

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+     <h1>Welcome to visit Yingqi's page</h1>
      <h3>Welcome to visit Yingqi's page</h3>
     </div>
   );
