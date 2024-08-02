b5cab8d70407e9bacf2ea6069de3ac7f233c3c8d
yingqi955 <yingqi.ch@gmail.com>
2024-08-02 19:42:31 -0400

    Adding modes and profile picture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import {Button, ButtonGroup, Container, Row, Col} from './import.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to visit Yingqi's page</h1>
-     <h3>Welcome to visit Yingqi's page</h3>
+      <Container>
+        <Row>
+          <Col>
+          <img src="/photos/profile-pic.jpg" alt="Profile"></img>
+          </Col>
+          <Col xs={8}>1 0f 2</Col>
+          <Col>
+          {/* button */}
+            <ButtonGroup aria-label = "modes">
+            <Button variant = "secondary">Light</Button>
+            <Button variant = "secondary">Dark</Button>
+          </ButtonGroup>
+          </Col>
+        </Row>
+      </Container>
+
     </div>
   );
 }
---
+++ src/import.js
@@ -0,0 +1,8 @@
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+export { Button, ButtonGroup, Container, Row, Col};
